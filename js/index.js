ef22e34d85260a6b6ee9cb529e21c8bdcd7cf8db
bplogan <bstyles@gmail.com>
2015-05-26 20:33:58 -0300

    sdf

--- js/index.js
+++ js/index.js
@@ -33,12 +33,12 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-    	app.receivedEvent('deviceready');
-    		if (navigator.userAgent.match(/(iPad.*|iPhone.*|iPod.*);.*CPU.*OS 7_\d/i)) {
+    	if (navigator.userAgent.match(/(iPad.*|iPhone.*|iPod.*);.*CPU.*OS 7_\d/i)) {
+    		alert("IOS");
 			$("body").addClass("ios7");
 			$("body").append('');
 		
-			}
+		}
        	StatusBar.overlaysWebView(false);
         
         
