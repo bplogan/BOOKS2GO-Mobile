18620a5a3efe62e23de8b78393720debc62e7af9
bplogan <loganb@RtTech-Brad2.rttechdev.local>
2015-06-10 14:05:17 -0300

    asd
    Conflicts:
    	index.html

--- js/index.js
+++ js/index.js
@@ -19,15 +19,14 @@
 var app = {
     // Application Constructor
     initialize: function() {
-    	alert("4");
-        this.bindEvents();
+          this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-    	alert("5");
+    	
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
@@ -35,148 +34,20 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-    	alert("6");
-    	//StatusBar.overlaysWebView(false);
-        app.receivedEvent('deviceready');
+    	
+    	var uuid = device.uuid;
+    	StatusBar.overlaysWebView(false);
+        if(PushbotsPlugin.isiOS()){
+			PushbotsPlugin.initializeiOS("553fa49017795918698b4571");
+		}
+		if(PushbotsPlugin.isAndroid()){
+			PushbotsPlugin.initializeAndroid("553fa49017795918698b4571", "758601829939");
+		}
+		setTimeout(function(){ 
+			PushbotsPlugin.setAlias(uuid);
+		}, 3000);
         
     },
-    tokenHandler:function(msg) {
-    	alert("7");
-       	//alert("Token Handler " + msg);
-       	if(window.localStorage.getItem("user_id") !== null && window.localStorage.getItem("user_id") !== undefined){
-		var UIDt = window.localStorage.getItem("user_id");
-		alert("8");
-			if(UIDt > 0){       
-				alert("9");
-	       	  $.ajax({
-		        type: "POST",
-		        url: "http://www.books2go.ca/mobiservice/notification.php?reg=1",
-		        async: false,
-		        data: {'UUID' : msg, 'UID' : UIDt, 'platform' : 'ios'},
-		        dataType: "json",
-		        success: function(data) {
-		        	alert("10");
-		        	//alert(data.test);
-		        	window.localStorage.setItem("UUID",msg);
-				    window.localStorage.setItem("PLATFORM", 'ios');
-				    alert("11");
-		        	window.location = "profile.html";
-		     	},
-		 	 	error: function(xhr, desc, err) {
-		        alert(xhr + " Details: " + desc + "\nError:" + err, $("#err"), 0);
-		       }
-		   	});         
-	   	}else{
-	   		alert("not logged in");
-	   		window.location = "main.html";
-	   	}	
-	   	}else{
-	   		alert("not logged in");
-	   		window.location = "main.html";
-	   		}	
-       	
-       	
-    },
-    errorHandler:function(error) {
-        console.log("Error Handler  " + error);
-       alert("error");
-    },
-    // result contains any message sent from the plugin call
-    successHandler: function(result) {
-      alert("success");
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-    	
-        var pushNotification = window.plugins.pushNotification;
-        // TODO: Enter your own GCM Sender ID in the register call for Android
-        if (device.platform == 'android' || device.platform == 'Android') {
-            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"758601829939","ecb":"app.onNotificationGCM"});
-        }
-        else {
-        	alert("registering");
-            pushNotification.register(this.tokenHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
-        }
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-        alert("penis?");
-    },
-    // iOS
-    onNotificationAPN: function(event) {
-        var pushNotification = window.plugins.pushNotification;
-        console.log("Received a notification! " + event.alert);
-        console.log("event sound " + event.sound);
-        console.log("event badge " + event.badge);
-        console.log("event " + event);
-        if (event.alert) {
-            navigator.notification.alert(event.alert);
-        }
-        if (event.badge) {
-            console.log("Set badge on  " + pushNotification);
-            pushNotification.setApplicationIconBadgeNumber(this.successHandler, event.badge);
-        }
-        if (event.sound) {
-            var snd = new Media(event.sound);
-            snd.play();
-        }
-        var urlios = event.url;
-        window.location = urlios;
-    },
-    // Android
-    onNotificationGCM: function(e) {
-        switch( e.event )
-        {
-            case 'registered':
-                if ( e.regid.length > 0 )
-                {
-                    // Your GCM push server needs to know the regID before it can push to this device
-                    // here is where you might want to send it the regID for later use.
-                    //alert('registration id = '+e.regid);
-                    var UID = window.localStorage.getItem("user_id");
-                    if(UID > 0){
-                    $.ajax({
-				        type: "POST",
-				        url: "http://www.books2go.ca/mobiservice/notification.php?reg=1",
-				        async: false,
-				        data: {'UUID' : e.regid, 'UID' : UID, 'platform' : 'android'},
-				        dataType: "json",
-				        success: function(data) {
-				        	window.localStorage.setItem("UUID",e.regid);
-				        	window.localStorage.setItem("PLATFORM", 'android');
-				        	window.location = "profile.html";
-				     	},
-				 	 	error: function(xhr, desc, err) {
-				        alert(xhr + " Details: " + desc + "\nError:" + err, $("#err"), 0);
-				       }
-				   	});
-				   	}else{
-				   		window.location = "main.html";
-				   	}		
-                }
-            break;
-
-            case 'message':
-              // this is the actual push notification. its format depends on the data model
-              // of the intermediary push server which must also be reflected in GCMIntentService.java
-              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-              var urldroid = e.url;
-              window.location = urldroid;
-            break;
-
-            case 'error':
-              alert('GCM error = '+e.msg);
-            break;
-
-            default:
-              alert('An unknown GCM event has occurred');
-              break;
-        }
-    }
+    
 
 };
